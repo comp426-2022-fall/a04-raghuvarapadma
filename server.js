d680540227ab33f531e56ba6870abe46cc86f13f
Raghuvara Padma <raghuvarapadma@gmail.com>
2022-11-16 19:02:10 -0500

    added line back

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import minimist from "minimist"
 import { roll } from "./lib/roll.js"
 
 const app = express();
+app.use(express.urlencoded({extended: true}))
 const args = minimist(process.argv.slice(2));
 
 let port = 5000;
